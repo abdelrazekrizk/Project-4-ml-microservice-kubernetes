5c5bb8a54c8be29956bc33875033d0f8dff53358
abdelrazek.rizk <abdelrazek.rizk@hotmail.com>
2021-10-21 19:52:22 +0200

    add screen shot

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import logging
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 
-import sklearn
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
@@ -21,8 +20,10 @@ def scale(payload):
 
 @app.route("/")
 def home():
+    """home APP."""
     html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
+
 
 @app.route("/predict", methods=['POST'])
 def predict():
