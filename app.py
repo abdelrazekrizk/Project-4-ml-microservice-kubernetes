84842ef9604ceb44630bd4f8bb5b89ab2ea31310
abdelrazek.rizk <abdelrazek.rizk@hotmail.com>
2021-10-21 07:45:40 +0200

    start pylint

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 
-
+import sklearn
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
